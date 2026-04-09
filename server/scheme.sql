7f3456ef6bea40dc1c799fc9b3c084fb5b4910d0
mwplace <hype5opka@gmail.com>
2026-04-09 15:38:35 +0300

    Update scheme.sql

--- server/scheme.sql
+++ server/scheme.sql
@@ -1,1 +1,9 @@
-
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    oauth_id VARCHAR(255) UNIQUE NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    droplets INT DEFAULT 0,
+    level INT DEFAULT 1,
+    max_charges INT DEFAULT 5,
+    last_charge_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
